1183dd7474b222ef683c7cb744eec81024bf6bc0
büşra DEMİREL <busrademirell549@gmail.com>
2025-11-24 23:45:24 +0300

    Display time-of-day message

--- welcome-screen/welcome-screen.js
+++ welcome-screen/welcome-screen.js
@@ -1,3 +1,16 @@
 let firstName = prompt("Please enter your name : ");
 let greeting = document.querySelector("#greeting");
 greeting.innerHTML = `${greeting.innerHTML} <small>${firstName}!</small>`;
+
+let currentHour = new Date().getHours();
+let info = document.querySelector("#info");
+
+if (currentHour < 12) {
+  info.innerHTML = "Good morning";
+} else if (currentHour < 18) {
+  info.innerHTML = "Good Afternoon";
+} else if (currentHour < 23) {
+  info.innerHTML = "Good Evening";
+} else {
+  info.innerHTML = "Good Night";
+}
