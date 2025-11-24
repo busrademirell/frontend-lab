66119edc83288ae3efff03dcc0ee613022b0f381
büşra DEMİREL <busrademirell549@gmail.com>
2025-11-24 23:45:45 +0300

    Implement real-time clock and date

--- welcome-screen/welcome-screen.js
+++ welcome-screen/welcome-screen.js
@@ -14,3 +14,33 @@ if (currentHour < 12) {
 } else {
   info.innerHTML = "Good Night";
 }
+function showTime() {
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  // Convert single digit numbers to 2 digits (like 09)
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  document.getElementById("clock").innerHTML = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(showTime, 1000);
+
+showTime(); // Run as soon as the page opens
+
+function showDate() {
+  const now = new Date();
+
+  const day = now.getDate();
+  const month = now.getMonth() + 1;
+  const year = now.getFullYear();
+
+  document.getElementById("date").innerHTML = `${day}.${month}.${year}`;
+}
+setInterval(showDate, 1000);
+showDate();
