a58e44bef4c0fa4ffa2ca4afa13237525e19c60d
büşra DEMİREL <busrademirell549@gmail.com>
2026-01-12 14:07:23 +0300

    Add buggy app exercise

---
+++ freecodecamp-javascript/booleans-and-numbers/debug-type-coercion-errors-in-a-buggy-app/script.js
@@ -0,0 +1,47 @@
+/*
+User Stories:
+
+You should have a variable named firstResult that correctly adds the numbers 5 and 10 to produce the value 15.
+You should have a variable named secondResult that correctly subtracts 5 from 8 to produce the value 3.
+You should have a variable named thirdResult that produces the value 6 by adding two numbers. Replace the boolean currently used in the expression with a number.
+You should have a variable named fourthResult that produces the value 8 by adding two numbers. Replace the boolean currently used in the expression with a number.
+You should have a variable named fifthResult that correctly multiplies two numbers to get the product 20. Replace the string currently used in the expression with a number.
+You should have a variable named sixthResult that correctly adds two numbers to produce the value 22. Replace the null currently used in the expression with a number.
+*/
+/* 
+Tests
+Waiting:1. You should have a variable called firstResult.
+Waiting:2. firstResult should have the numeric value 15.
+Waiting:3. You should have a variable called secondResult.
+Waiting:4. secondResult should have the numeric value 3.
+Waiting:5. You should have a variable called thirdResult.
+Waiting:6. thirdResult should have the numeric value 6.
+Waiting:7. thirdResult should not include a boolean value (true or false).
+Waiting:8. You should have a variable called fourthResult.
+Waiting:9. fourthResult should have the numeric value 8.
+Waiting:10. fourthResult should not include a boolean value (true or false).
+Waiting:11. You should have a variable called fifthResult.
+Waiting:12. fifthResult should have the numeric value 20.
+Waiting:13. fifthResult should not include a string multiplication (e.g. "10" * 2).
+Waiting:14. You should have a variable called sixthResult.
+Waiting:15. sixthResult should have the numeric value 22.
+Waiting:16. sixthResult should not include null in its expression.
+*/
+
+const firstResult = 10 + 5;
+console.log(`10 + 5 = ${firstResult}`);
+
+const secondResult = "8" - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 1 + 5;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 0 + 8;
+console.log(`0 + 8 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 0 + 22;
+console.log(`0 + 22 = ${sixthResult}`);
